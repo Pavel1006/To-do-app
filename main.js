32ebb030b666ebf470c6c0f6bbaa35d4ae964d53
github_username <email_address>
2021-03-02 14:31:41 +0200

    first task

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.getElementById("button").onclick = function task() {
   let inptValue = document.getElementById("firstImp").value;
-  return (document.getElementById("task").innerHTML = inptValue);
+  let taskInpt = (document.getElementById("task").innerHTML = inptValue);
+  let text =
+    ' <div><input id="border" type="checkbox"><span> value</span><button type="button">delete</button></div><br>';
+
+  return (document.getElementById("task").innerHTML = text);
 };
