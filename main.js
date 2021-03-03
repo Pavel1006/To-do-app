3fcd12cc794c98754d92e9c60aa43ae78867b1ed
github_username <email_address>
2021-03-03 11:31:00 +0200

    make first task

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
-document.getElementById("button").onclick = function task() {
-  let inptValue = document.getElementById("firstImp").value;
+/*function task() {
+  let inptValue = document.getElementById("firstImp").text;
   let text =
     ' <div id="border"><input type="checkbox"><span id="val"> test 1</span><button id="delete" type="button">delete</button></div>';
-  /* document.getElementById("val").innerHTML = inptValue;
+  let v = (document.getElementById("val").innerHTML = inptValue);
   if (document.getElementById("delete").onclick) {
    return (document.getElementById("delete").style.display = "none");
   }
-  */
-  return (document.getElementById("task").innerHTML = text);
-};
+  
+  return document.write(v);
+  (document.getElementById("task").innerHTML = text);
+}
+document.getElementById("button").onclick = task;
+document.getElementById("delete").onclick = function sterge() {
+  return (document.getElementById("delete").style.display = "none");
+};*/
+
+//create the elements
+let div = document.createElement("div");
+let span = document.createElement("span");
+let input = document.createElement("input");
+let button = document.createElement("button");
+//create the atributes
+let idDiv = document.createAttribute("id");
+let idSpan = document.createAttribute("id");
+let idButton = document.createAttribute("id");
+let inpCheck = document.createAttribute("type");
+let buttonAtr = document.createAttribute("type");
+let inptValue = document.getElementById("firstImp").value;
+//set the value of atributes
+idDiv.value = "border";
+idSpan.value = "val";
+idButton.value = "delete";
+inpCheck.value = "checkbox";
+buttonAtr.value = "button";
+//set how's atribute is
+div.setAttributeNode(idDiv);
+span.setAttributeNode(idSpan);
+input.setAttributeNode(inpCheck);
+button.setAttributeNode(idButton);
+button.setAttributeNode(buttonAtr);
+//applend the text
+span.appendChild(inptValue);
+button.appendChild(document.createTextNode("delete"));
